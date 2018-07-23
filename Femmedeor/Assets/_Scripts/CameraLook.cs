83db2d47fca145fe91391b87a1297cef544e856d
Ovetgana <verbo0000@gmail.com>
2018-07-23 18:44:05 +0300

    Change control to more convenient

---
+++ Femmedeor/Assets/_Scripts/CameraLook.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Player
+{
+    public class MouseCamera
+    {
+        public float XSensitivity = 2f;
+        public float YSensitivity = 2f;
+
+        private Quaternion m_CharacterTargetRot;
+
+        public void Init(Transform character)
+        {
+            m_CharacterTargetRot = character.localRotation;
+        }
+
+        public void LookRotation(Transform character)
+        {
+            float yRot = Input.GetAxis("Mouse X") * XSensitivity;
+            float xRot = Input.GetAxis("Mouse Y") * YSensitivity;
+           
+            Vector3 rotation_y_global = new Vector3(0f, yRot, 0f);
+
+            // transformation rotation around Y-axis from global to local coordinates in order to keep bottom edge of camera parallel to the horizon
+            Vector3 rotation_y_local = character.transform.InverseTransformVector(rotation_y_global);
+            
+            m_CharacterTargetRot *= Quaternion.Euler(rotation_y_local) * Quaternion.Euler(-xRot, 0, 0f);
+            
+            character.localRotation = m_CharacterTargetRot;
+        }
+    }
+
+    
+
+}
--- Femmedeor/Assets/_Scripts/PlayerController.cs
+++ Femmedeor/Assets/_Scripts/PlayerController.cs
@@ -2,43 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
 
-    public float moving_speed = 50f;
-    public float rotation_speed = 5f;
+namespace Player
+{
+    public class PlayerController : MonoBehaviour {
 
-    private Vector3 movement_local;
-    private Quaternion rotation;
-    private Rigidbody playerRigidbody;
+        public MouseCamera mouseCamera = new MouseCamera ();
+        public float impulse_rate = 30f;
+      
+        private Vector3 impulse_local;
+        private Rigidbody playerRigidbody;
 
 
-    void Awake()
-    {
-        playerRigidbody = GetComponent<Rigidbody>();
+        void Start()
+        {
+            playerRigidbody = GetComponent<Rigidbody>();
+            mouseCamera.Init(transform);
+        }
+
+        void FixedUpdate()
+        {
+            float h = Input.GetAxis("Horizontal");   // moving left/right, keys "a"/"d"
+            float v = Input.GetAxis("Vertical");    // moving forward/backward, keys "w"/"s"
+            float u = Input.GetAxis("Up");          // moving up/down, keys "q"/"e"
+            Move(h, u, v);
+
+        }
+
+        void Move(float h, float u, float v)
+        {
+            impulse_local.Set(h, 0f, v);
+            impulse_local += transform.InverseTransformVector(new Vector3(0f, u, 0f));    // up/down direction should be independent of local frame's orientation
+            impulse_local = impulse_local.normalized * impulse_rate * Time.deltaTime;
+
+            // transformation from local to global coordinate frame due to keeping correct moving after rotation
+            Vector3 impulse__global = transform.TransformVector(impulse_local);
+            
+            // applying mooving and rotation
+            playerRigidbody.AddForce(impulse__global, ForceMode.Impulse);
+            mouseCamera.LookRotation(transform);
+        }
     }
 
-    void FixedUpdate()
-    {
-        float h = Input.GetAxis("Horizontal");   // moving left/right, keys "a"/"d"
-        float v = Input.GetAxis("Vertical");    // moving forward/backward, keys "w"/"s"
-        float u = Input.GetAxis("Up");          // moving up/down, keys "left shift"/"left ctrl"
-        float r = Input.GetAxis("Rotation");    // rotations to the right/left, keys "e"/"q"
-        Move(h, u, v, r);
-        
-    }
-
-    void Move(float h, float u, float v, float r)
-    {
-        movement_local.Set(h, u, v);
-        movement_local = movement_local.normalized * moving_speed * Time.deltaTime;
-
-        // transformation from local to global coordinate frame due to keeping correct moving after rotation
-        Vector3 movement__global = transform.TransformVector(movement_local);    
-
-        rotation = Quaternion.Euler(new Vector3(0.0f, r, 0.0f).normalized * rotation_speed * Time.deltaTime);
-
-        // applying mooving and rotation
-        playerRigidbody.MovePosition(transform.position + movement__global);
-        playerRigidbody.MoveRotation(playerRigidbody.rotation * rotation);
-    }
 }
